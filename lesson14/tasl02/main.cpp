3c08c878ebb8a0e3a1dc28eda4f8ccef92292262
Abstraction1 <humancollection@gmail.com>
2017-09-03 13:54:54 +0300

    lessons

---
+++ lesson14/tasl02/main.cpp
@@ -0,0 +1,19 @@
+//Показать на экран с m по n символов строки, введенной пользователем и записать данный отрезок в другой массив. (m и n также вводятся пользователем)
+//
+#include <iostream>
+
+int main()
+{
+    int m = 5;
+    int n = 10;
+    int i = 0;
+    char symb[100] = {"qwertasdfg"};
+    char* s;
+    while (m < n) {
+        *(s+i) = *(symb+m);
+        i++;m++;
+    }
+    std::cout <<s;
+
+    return EXIT_SUCCESS;
+}
